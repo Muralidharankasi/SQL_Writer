0c19aca6c1557cbc46d2a19d594fbc401895ca0a
Muralidharankasi <46962502+Muralidharankasi@users.noreply.github.com>
2020-08-18 18:42:25 +0530

    SQL_MAKER Version- 1.0

---
+++ sql_insert/src/sql_Insert_maker/sql_insert.java
@@ -0,0 +1,86 @@
+package sql_Insert_maker;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+public class sql_insert {
+
+	public static void main(String[] args) throws IOException {
+			
+		  String name = "C:\\Users\\murali-9880\\Desktop\\sql.txt";
+		  @SuppressWarnings("resource")
+		Scanner scan = new Scanner (System.in);
+		  String st; 
+		  System.out.println("Enter Table Name");
+		  String table_name=scan.nextLine();
+		  Integer IsTrial = 0;
+		  System.out.println("Review the generated SQL Query");
+		 while(true) {
+			 BufferedReader br = getInfo(name);
+			 while ((st = br.readLine()) != null) 
+			  {
+				 String Query = makeQuery(st,table_name);
+				if(IsTrial!=1)
+					System.out.println(Query); 
+				if(IsTrial==1 && !Query.isEmpty())
+				writeIntoFile(Query);
+			  } 
+			 if(IsTrial==1) {
+				 break;
+			 }
+			 System.out.println();
+			 System.out.println();
+			 System.out.println("If Everything Okay, Enter 1 to Write into SQL file");
+			 IsTrial = scan.nextInt();
+		 }
+		 System.out.println("COMPLETED.....");
+  } 
+	
+	static void writeIntoFile(String input) {
+		 try{    
+			  FileWriter fw = new FileWriter(new File("C:\\Users\\murali-9880\\Desktop\\dlp_dump_detail.sql"), true);
+			  BufferedWriter br1 = new BufferedWriter(fw);
+			  PrintWriter pr = new PrintWriter(br1);
+			  pr.println(input);
+			  pr.close();
+			  br1.close();
+	          }catch(Exception e){System.out.println(e);}    
+        
+	}
+	
+	static BufferedReader getInfo(String file) throws FileNotFoundException {
+		File file1 = new File(file); 
+		BufferedReader br1 = new BufferedReader(new FileReader(file1)); 
+		return br1;
+	}
+	
+	private static String makeQuery(String st, String table_name) {
+		st = st.replaceAll("\\s{2,}", " ");
+		String array[]=st.split("	");
+		String query=  "INSERT INTO "+table_name+" VALUES(";
+		boolean start = false;
+		for(String s:array){
+			if(start)
+				query+=",";
+			try {
+				if(s.contains("'"))
+				s=s.replace("'","''");
+				query+=s;
+			}catch (Exception E) {query+="'"+s+"'";}
+			start = true;
+		}
+		query+=");";
+		return query;
+	}
+}
