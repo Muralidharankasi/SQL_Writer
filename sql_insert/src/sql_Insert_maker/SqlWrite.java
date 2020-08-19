fd3f620a775228b8356457afc02e073bb0cd21b9
Muralidharankasi <46962502+Muralidharankasi@users.noreply.github.com>
2020-08-19 11:00:04 +0530

    Update and rename sql_insert.java to SqlWrite.java

--- sql_insert/src/sql_Insert_maker/SqlWrite.java
+++ sql_insert/src/sql_Insert_maker/SqlWrite.java
@@ -16,45 +16,45 @@ import com.sun.javafx.collections.MappingChange.Map;
 /*
 		Author ILARUM SQL_Writer.sql generator
 */
-public class sql_insert {
+public class SqlWriter {
 
 	public static void main(String[] args) throws IOException {
 			
-		  String name = "D:\\sql.txt";
-		  @SuppressWarnings("resource")
+		  String source_file = "D:\\sql.txt";
 		Scanner scan = new Scanner (System.in);
 		  String st; 
 		  System.out.println("Enter Table Name");
-		  String table_name=scan.nextLine();
-		  Integer IsTrial = 0;
+		  String tableName=scan.nextLine();
+		  int isTrial = 0;
 		  System.out.println("Review the generated SQL Query");
 		 while(true) {
-			 BufferedReader br = getInfo(name);
+			 BufferedReader br = getInfo(source_file);
 			 while ((st = br.readLine()) != null) 
 			  {
-				 String Query = makeQuery(st,table_name);
-				if(IsTrial!=1)
+				 String Query = makeQuery(st,tableName);
+				if(isTrial!=1)
 					System.out.println(Query); 
-				if(IsTrial==1 && !Query.isEmpty())
+				if(isTrial==1 && !Query.isEmpty())
 				writeIntoFile(Query);
 			  } 
-			 if(IsTrial==1) {
+			 if(isTrial==1) {
 				 break;
 			 }
 			 System.out.println();
 			 System.out.println();
 			 System.out.println("If Everything Okay, Enter 1 to Write into SQL file");
-			 IsTrial = scan.nextInt();
+			 isTrial = scan.nextInt();
 		 }
+		 scan.close();
 		 System.out.println("COMPLETED.....");
   } 
 	
-	static void writeIntoFile(String input) {
+	static void writeIntoFile(String Query) {
 		 try{    
 			  FileWriter fw = new FileWriter(new File("D:\\SQL_Writer.sql"), true);
 			  BufferedWriter br1 = new BufferedWriter(fw);
 			  PrintWriter pr = new PrintWriter(br1);
-			  pr.println(input);
+			  pr.println(Query);
 			  pr.close();
 			  br1.close();
 	          }catch(Exception e){System.out.println(e);}    
@@ -67,10 +67,10 @@ public class sql_insert {
 		return br1;
 	}
 	
-	private static String makeQuery(String st, String table_name) {
+	private static String makeQuery(String st, String tableName) {
 		st = st.replaceAll("\\s{2,}", " ");
 		String array[]=st.split("	");
-		String query=  "INSERT INTO "+table_name+" VALUES(";
+		String query=  "INSERT INTO "+tableName+" VALUES(";
 		boolean start = false;
 		for(String s:array){
 			if(start)
